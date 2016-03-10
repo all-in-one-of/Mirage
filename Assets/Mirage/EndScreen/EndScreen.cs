618af33b1b592a99e1da4ce43b51aaaa3f37a26e
Keijiro Takahashi <keijiro@ZX.local>
2016-03-10 22:32:32 +0900

    Add EndScreen

---
+++ Assets/Mirage/EndScreen/EndScreen.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class EndScreen : MonoBehaviour
+{
+    [SerializeField] float _blinkInterval = 0.3f;
+    [SerializeField, HideInInspector] Texture _texture0;
+    [SerializeField, HideInInspector] Texture _texture1;
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        var t = Time.time / _blinkInterval % 2 > 1 ? _texture0 : _texture1;
+        Graphics.Blit(t, destination);
+    }
+}
