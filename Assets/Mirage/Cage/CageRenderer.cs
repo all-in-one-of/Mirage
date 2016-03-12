cce3d4b10428315d3c07fda8cdb6c291a8d9690e
Keijiro Takahashi <keijiro@ZX.local>
2016-03-13 00:18:40 +0900

    Add OSC rig.

--- Assets/Mirage/Cage/CageRenderer.cs
+++ Assets/Mirage/Cage/CageRenderer.cs
@@ -9,28 +9,51 @@ namespace Mirage
         #region Exposed Parameters
 
         [SerializeField]
-        CageMesh _mesh;
-
-        [Space]
-        [SerializeField]
         float _radius = 1.0f;
+
+        public float radius {
+            get { return _radius; }
+            set { _radius = value; }
+        }
 
         [SerializeField]
         float _noiseAmplitude = 0.05f;
 
+        public float noiseAmplitude {
+            get { return _noiseAmplitude; }
+            set { _noiseAmplitude = value; }
+        }
+
         [SerializeField]
         float _noiseFrequency = 1.0f;
+
+        public float noiseFrequency {
+            get { return _noiseFrequency; }
+            set { _noiseFrequency = value; }
+        }
 
         [SerializeField]
         float _noiseMotion = 0.2f;
 
-        [Space]
+        public float noiseMotion {
+            get { return _noiseMotion; }
+            set { _noiseMotion = value; }
+        }
+
         [SerializeField, ColorUsage(false, true, 0, 8, 0.125f, 3)]
         Color _lineColor = Color.white;
+
+        public Color lineColor {
+            get { return _lineColor; }
+            set { _lineColor = value; }
+        }
 
         #endregion
 
         #region Private Resources
+
+        [SerializeField, HideInInspector]
+        CageMesh _mesh;
 
         [SerializeField, HideInInspector]
         Shader _lineShader;
