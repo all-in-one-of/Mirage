5dbfba31574cb0b9dd466bddb1918a223694aa8d
Keijiro Takahashi <keijiro@ZX.local>
2016-03-19 22:25:49 +0900

    Add StripeFx

---
+++ Assets/Mirage/StripeFx/StripeFx.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using Klak.MaterialExtension;
+
+namespace Mirage
+{
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(Camera))]
+    public class StripeFx : MonoBehaviour
+    {
+        #region Exposed properties
+
+        public float cutoff {
+            get { return _cutoff; }
+            set { _cutoff = value; }
+        }
+
+        [SerializeField, Range(0, 1)] float _cutoff = 0.5f;
+
+        public Transform origin {
+            get { return _origin; }
+            set { _origin = value; }
+        }
+
+        [Space, SerializeField] Transform _origin;
+
+        public float frequency {
+            get { return _frequency; }
+            set { _frequency = value; }
+        }
+
+        [SerializeField] float _frequency = 0.3f;
+
+        public float speed {
+            get { return _speed; }
+            set { _speed = value; }
+        }
+
+        [SerializeField] float _speed = 1;
+
+        public Color color {
+            get { return _color; }
+            set { _color = value; }
+        }
+
+        [Space, SerializeField, ColorUsage(false, true, 0, 8, 0.125f, 3)]
+        Color _color = Color.white;
+
+        public float specular {
+            get { return _specular; }
+            set { _specular = value; }
+        }
+
+        [SerializeField] float _specular = 2;
+
+        #endregion
+
+        #region Private resources
+
+        [SerializeField, HideInInspector] Shader _shader;
+        Material _material;
+
+        #endregion
+
+        #region Private properties and variables
+
+        float _offset;
+
+        #endregion
+
+        #region MonoBehaviour functions
+
+        void OnEnable()
+        {
+            _material = new Material(Shader.Find("Hidden/Mirage/Stripe"));
+            _material.hideFlags = HideFlags.DontSave;
+        }
+
+        void OnDisable()
+        {
+            DestroyImmediate(_material);
+            _material = null;
+        }
+
+        void Update()
+        {
+            _offset += _speed * Time.deltaTime;
+        }
+
+        void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            var spos = _origin != null ? _origin.position : Vector3.zero;
+            var sdir = _origin != null ? _origin.up : Vector3.up;
+
+            _material.
+                Property("_Color", _color).
+                Property("_Specular", _specular).
+                Property("_Frequency", _frequency).
+                Property("_Cutoff", _cutoff).
+                Property("_Origin", spos).
+                Property("_Direction", sdir).
+                Property("_Offset", _offset);
+
+            Graphics.Blit(source, destination, _material, 0);
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Mirage/StripeFx/StripeFx.shader
@@ -0,0 +1,68 @@
+﻿Shader "Hidden/Mirage/Stripe"
+{
+    Properties
+    {
+        _MainTex("", 2D) = "" {}
+        _Color("", Color) = (1, 1, 1)
+    }
+
+    CGINCLUDE
+
+    #include "UnityCG.cginc"
+
+    sampler2D _MainTex;
+
+    sampler2D_float _CameraDepthTexture;
+    sampler2D _CameraGBufferTexture2;
+    float4x4 _WorldToCamera;
+
+    fixed3 _Color;
+    float _Specular;
+
+    float _Frequency;
+    float _Cutoff;
+    float3 _Origin;
+    float3 _Direction;
+    float _Offset;
+
+    half4 frag(v2f_img i) : SV_Target
+    {
+        // Sample a linear depth on the depth buffer.
+        float depth_s = SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture, i.uv);
+        float depth_o = LinearEyeDepth(depth_s);
+
+        // Sample a view-space normal vector on the g-buffer.
+        float3 norm_o = tex2D(_CameraGBufferTexture2, i.uv).xyz * 2 - 1;
+        norm_o = mul((float3x3)_WorldToCamera, norm_o);
+
+        // Reconstruct the view-space position.
+        float2 p11_22 = float2(unity_CameraProjection._11, unity_CameraProjection._22);
+        float2 p13_31 = float2(unity_CameraProjection._13, unity_CameraProjection._23);
+        float3 pos_o = float3((i.uv * 2 - float2(1, -1) - p13_31) / p11_22, 1) * depth_o;
+
+        // Rimlight
+        float rim = (depth_s < 1);
+        rim *= pow(1 - saturate(-dot(normalize(pos_o), norm_o)), _Specular);
+
+        // Stripe
+        float stripe = dot(pos_o - _Origin, _Direction);
+        stripe = frac(stripe * _Frequency - _Offset) < _Cutoff;
+
+        return lerp(0, fixed4(_Color, 1), rim * stripe);
+    }
+
+    ENDCG
+
+    SubShader
+    {
+        Pass
+        {
+            ZTest Always Cull Off ZWrite Off
+            CGPROGRAM
+            #pragma vertex vert_img
+            #pragma fragment frag
+            #pragma target 3.0
+            ENDCG
+        }
+    }
+}
