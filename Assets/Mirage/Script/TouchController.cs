32568828a2129b9464f124510d980b34a7170d24
Keijiro Takahashi <keijiro@ZX.local>
2016-04-08 23:21:22 +0900

    Implement TouchController.

--- Assets/Mirage/Script/TouchController.cs
+++ Assets/Mirage/Script/TouchController.cs
@@ -1,11 +1,48 @@
 using UnityEngine;
+using System.Collections.Generic;
+using Klak.Motion;
 
 namespace Mirage
 {
     public class TouchController : MonoBehaviour
     {
+        [SerializeField] SmoothFollow[] _followers;
+
+        [Space]
+
         [SerializeField] Transform _frontScreen;
         [SerializeField] Camera _primaryCamera;
+
+        class FollowerHandler
+        {
+            SmoothFollow _follower;
+            Transform _originalTarget;
+
+            public FollowerHandler(SmoothFollow follower)
+            {
+                _follower = follower;
+                _originalTarget = follower.target;
+            }
+
+            public void UseOriginalTarget()
+            {
+                _follower.target = _originalTarget;
+            }
+
+            public void ChangeTarget(Transform target)
+            {
+                _follower.target = target;
+            }
+        }
+
+        List<FollowerHandler> _handlers;
+
+        void Start()
+        {
+            _handlers = new List<FollowerHandler>(_followers.Length);
+            foreach (var follower in _followers)
+                _handlers.Add(new FollowerHandler(follower));
+        }
 
         void Update()
         {
@@ -21,7 +58,13 @@ namespace Mirage
                 px = (px - 0.5f) * scale.x;
                 py *= scale.y;
 
-                transform.position = new Vector3(px, py, 0);
+                transform.localPosition = new Vector3(px, py, 0);
+
+                foreach (var h in _handlers) h.ChangeTarget(transform);
+            }
+            else
+            {
+                foreach (var h in _handlers) h.UseOriginalTarget();
             }
         }
     }
