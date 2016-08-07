5edc337b6b40d88a67a9af7e174bd27e45744963
Keijiro Takahashi <keijiro@gmail.com>
2016-08-07 21:57:41 +0900

    Default value and kill switch

--- Assets/Mirage/Script/KillSwitch.cs
+++ Assets/Mirage/Script/KillSwitch.cs
@@ -6,10 +6,12 @@ namespace Mirage
     public class KillSwitch : MonoBehaviour
     {
         [SerializeField] Slider[] _slidersToKill;
+        [SerializeField] Button[] _buttonsToClick;
 
         public void KillAllEffects()
         {
             foreach (var slider in _slidersToKill) slider.value = 0;
+            foreach (var button in _buttonsToClick) button.onClick.Invoke();
         }
     }
 }
